dcfbcf342b6dba767d75effa248f4b9d92d29e19
dotnetappdev <dotnetappdev@users.noreply.github.com>
2026-07-18 22:50:31 +0000

    deploy: c88f60d4785546d94856f1c873cab8419459bd46

---
+++ allure-report/assets/chunks/swimlanesDiagram-G3AALYLV.BJqYr6MG.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.DnANLVEA.js";import{_ as a}from"../app.yXDJamOR.js";import"./chunk-5VM5RSS4.0d5Z_gz0.js";import"./chunk-XXDRQBXY.D6av_Jmf.js";import"./chunk-VR4S4FIN.8YBvROal.js";import"./chunk-32BRIVSS.0wJAd0IQ.js";import"./channel.BOGaLaYt.js";import"./framework.BkRhkvTH.js";import"./theme.aZwmnMbJ.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
